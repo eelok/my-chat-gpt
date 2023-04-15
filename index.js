c3bc061199394bf9d97bd1f766481996ae653f4d
eelok <maria@resourceful-humans.de>
2023-04-15 17:41:06 +0200

    add cors

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const { openAPIconfig } = require('./openAIConfig');
 require('dotenv').config();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 
 const port = process.env.PORT || 5500;
